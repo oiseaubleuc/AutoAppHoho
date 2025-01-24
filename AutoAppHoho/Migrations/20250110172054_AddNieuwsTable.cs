e815ab2088496174a9f3d6d2dec9917dc6c5417c
Houdaifa Hamouchi <houdaifa.hamouchi@student.ehb.be>
2025-01-24 22:13:31 +0100

    advanced

--- AutoAppHoho/Migrations/20250110172054_AddNieuwsTable.cs
+++ AutoAppHoho/Migrations/20250110172054_AddNieuwsTable.cs
@@ -5,10 +5,8 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace AutoAppHoho.Migrations
 {
-    /// <inheritdoc />
     public partial class AddNieuwsTable : Migration
     {
-        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
